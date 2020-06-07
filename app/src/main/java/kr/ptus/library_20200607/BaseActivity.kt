de0088e30a1a299c0bb2e832b6a2b687ed5f7ecf
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-07 10:29:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/ptus/library_20200607/BaseActivity.kt
@@ -0,0 +1,14 @@
+package kr.ptus.library_20200607
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+
+}
