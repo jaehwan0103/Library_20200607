23124759939b7611b2e5b551a721cb337c86a116
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-07 13:46:43 +0900

    tedPermission 라이브러리로 권한 체크 후 통화 실행

--- app/src/main/java/kr/ptus/library_20200607/MainActivity.kt
+++ app/src/main/java/kr/ptus/library_20200607/MainActivity.kt
@@ -1,11 +1,15 @@
 package kr.ptus.library_20200607
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -24,14 +28,30 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
 
         callBtn.setOnClickListener {
-            val myUri = Uri.parse("tel:${phoneNumTxt.text}")
-            val myIntent = Intent(Intent.ACTION_CALL,myUri)
 
-            startActivity(myIntent)
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:${phoneNumTxt.text}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(mContext,"권한이 거부되어 통화를 할 수 없습니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener).setDeniedMessage("거절되면 통화할 수 없으니 [설정]에서 권한을 허가해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE).check()
+
         }
 
         proflieImage.setOnClickListener {
-            val myIntent = Intent(mContext,ViewProfileImageActivity::class.java)
+            val myIntent = Intent(mContext, ViewProfileImageActivity::class.java)
             startActivity(myIntent)
 
         }
@@ -41,7 +61,9 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        Glide.with(mContext).load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQs5Dvhb_t65a84jxNdImgtkwb9TupgUG9Ww2xRxi7NeU3bPaui&usqp=CAU").into(proflieImage)
+        Glide.with(mContext)
+            .load("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQs5Dvhb_t65a84jxNdImgtkwb9TupgUG9Ww2xRxi7NeU3bPaui&usqp=CAU")
+            .into(proflieImage)
 
     }
 }
